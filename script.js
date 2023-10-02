098d94d7b2153e078267362072cc9ae9e6e49d2e
PrGhostDev <139947007+PrGhostDev@users.noreply.github.com>
2023-10-02 19:53:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+const sizeInfo = document.querySelector('#sizeInfo h1');
+
+function updateSizeInfo() {
+  const width = window.innerWidth;
+  const height = window.innerHeight;
+  sizeInfo.textContent = `Width: ${width} Height: ${height}`;
+}
+
+window.addEventListener('resize', updateSizeInfo);
+updateSizeInfo();
